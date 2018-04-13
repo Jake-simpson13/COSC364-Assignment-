04ed6806f9eb47b3bd65cd570e87eb06d84172d9
James Lilley <jli231@uclive.ac.nz>
2018-04-14 11:39:06 +1200

    constructors for packets to be sent and recieved

---
+++ packets.py
@@ -0,0 +1,73 @@
+""" 
+a class for the packet header, compplete with a field for 'payload'
+which is the router information to be used in the router table
+"""
+class Packet:
+    # command = command (1) (1 = request, 1 = response)
+    # version = version (1) (version of the rip protocol used - always 2)
+    # payload = RIP entries (20) (between 1 - 25 inc, otherwise multiple packets) 
+    
+    def __init__(self):
+        self.command = None
+        self.version = None
+        self.must_be_0 = '00'
+        self.p_payload = None
+        
+    def __init__(self, command, version, p_payload):
+        self.command = command
+        self.version = version
+        self.must_be_0 = '00'
+        self.p_payload = p_payload
+        
+    def packet(self):
+        value = str(self.command) + str(self.version)
+        for pay in self.p_payload:
+            value += str(pay.addr_fam_id)
+            value += str(pay.must_be_0_2)
+            value += str(pay.ipv4_addr)
+            value += str(pay.must_be_0_4)
+            value += str(pay.metric)
+        return value
+        
+""" 
+a class for the packet payload, which gets included in the 
+class packet as payload. Between 1 and 25 payload objects to one packet object        
+"""
+class Payload:
+    # addr_fam_id = address family identifier (2)
+    # ipv4_addr = IPv4 address (4)
+    # metric = metric (4) (cost - must be between 1 - 15 inc, 16 = inf / unreachable)
+    
+    def __init__(self):
+        self.addr_fam_id = None
+        self.must_be_0_2 = '00'
+        self.ipv4_addr = None
+        self.must_be_0_4 = '0000'
+        self.metric = None
+        
+    
+    def __init__(self, addr_fam_id, ipv4_addr, metric):
+        self.addr_fam_id = addr_fam_id
+        self.must_be_0_2 = '00'
+        self.ipv4_addr = ipv4_addr
+        self.must_be_0_4 = '0000'
+        self. metric = metric
+        
+        
+    def payload(self):
+        return str(self.addr_fam_id), self.must_be_0_2, str(self.ipv4_addr), self.must_be_0_4, str(self.metric)
+    
+
+
+
+'''
+#Test data
+
+x = Payload(2, 40, 7)
+print(x.payload())
+y = Payload(4, 30, 5)
+print(y.payload())
+ppayload = x,y
+z = Packet(1, 2, ppayload)
+print(z.packet())
+'''
