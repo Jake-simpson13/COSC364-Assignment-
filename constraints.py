055ff0960100f5ce7008e3fe465dbb42abadf97f
James Hugh Lilley <jli231@cs18200kq.canterbury.ac.nz>
2018-05-13 13:32:52 +1200

    beginning of programme, takesthe three parameters x, y, z and begins forming lp file

---
+++ constraints.py
@@ -0,0 +1,46 @@
+import sys
+
+FILENAME = "assignment2.lp"
+
+
+
+def header():
+    return "Minimize\n\tr\nSubject to\n"
+
+def constraints(x, y, z):
+    
+    constraints = ""
+    for i in range(1, x +1):
+        #n = i + k
+        for k in range(1, z+1):
+            for j in range(1, y+1):
+                n = i + j
+                if j < y:
+                    constraints += ("x{}{}{} + ".format(i, j, k))                    
+                else:
+                    constraints += ("x{}{}{} = {}\n".format(i, j, k, n))
+    return constraints                    
+    
+                    
+    
+
+
+def main(x, y, z):
+    lp = header()
+    lp += constraints(x, y, z)
+    print(lp)
+    
+    file = open(FILENAME, "w")
+    file.write(lp)
+    file.close()
+    
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        z = int(sys.argv[3])
+    else:
+        x = int(input("How many src nodes?: "))
+        y = int(input("How many trans nodes?: "))
+        z = int(input("How many dst nodes?: "))
+    main(x, y, z)
